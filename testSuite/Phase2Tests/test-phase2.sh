2e49a2f25d11e1b1a7b09bde8f36d38d9eafa3eb
jaredmcgrath <jared.mcgrath@queensu.ca>
2021-03-01 19:09:04 -0500

    Restructure testSuite

---
+++ testSuite/Phase2Tests/test-phase2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Testing Start - PHASE 2"
+
+# Make sure the output directory exists
+if [ ! -d "p2_out" ]; then
+  mkdir p2_out
+fi
+
+for i in *.pt
+do
+	ssltrace "ptc -o2 -t2 -L ../../src/lib/pt $i" ../../src/lib/pt/parser.def -e > p2_out/$i.eOutput
+	diff -b "$i.eOutput" "p2_out/$i.eOutput" > p2_out/$i.eOutputDiff
+done
+cd p2_out
+for i in *.eOutputDiff
+do
+	echo $i
+  cat $i
+done
