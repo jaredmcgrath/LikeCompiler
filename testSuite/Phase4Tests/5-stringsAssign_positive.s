c4e8ec0368228ae59b40831615b53ccd5759eed0
Travis Cossarini <16tjc3@linux6.caslab.queensu.ca>
2021-04-13 00:34:39 -0400

    done for the night, no major cahnges

--- testSuite/Phase4Tests/5-stringsAssign_positive.s
+++ testSuite/Phase4Tests/5-stringsAssign_positive.s
@@ -1,5 +1,5 @@
     .data    
-    .comm    u,4
+    .comm    u,260
 n:    .long    0
     .text    
     .globl    ptmain
@@ -7,11 +7,38 @@ ptmain:    pushl   %ebp
         movl    %esp, %ebp    
     movl    $2,u+0
     .data    
-s15:    .asciz  "string"
+s15:    .asciz  "strang1111111"
     .text    
     movl    $4,n
     pushl    u+0
     lea    s15,%eax
+    pushl    %eax
+    pushl    $0
+    call    pttrap109
+    addl    $12,%esp
+    incl    n
+    pushl    u+0
+    call    pttrap6
+    addl    $4,%esp
+    .data    
+s63:    .asciz  "otherstrang"
+    .text    
+    pushl    %eax
+    pushl    %ebx
+    pushl    %ecx
+    pushl    %edx
+    lea    u+4,%eax
+    pushl    %eax
+    lea    s63,%eax
+    pushl    %eax
+    call    pttrap101
+    popl    %edx
+    popl    %ecx
+    popl    %ebx
+    popl    %eax
+    movl    $8,n
+    pushl    u+0
+    lea    u+4,%eax
     pushl    %eax
     pushl    $0
     call    pttrap109
--- testSuite/Phase4Tests/test-single-sem.sh
+++ testSuite/Phase4Tests/test-single-sem.sh
@@ -81,7 +81,7 @@ ptc -o3 -t3 -L $pt_lib_path $src_path > $out_file_path
 # Next, append the marker to seperate ptc output from ssltrace output
 echo '### END OF PTC OUTPUT ###' >> $out_file_path
 # Finally, run ssltrace
-ssltrace "ptc -o3 -t3 -L $pt_lib_path $src_path" $pt_lib_path/semantic.def -e >> $out_file_path
+ssltrace "ptc -o3 -t3 -L $pt_lib_path $src_path" $pt_lib_path/semantic.def -t >> $out_file_path
 # out_asm_path="$out_dir/$(basename $src_path).s"
 # Generate executable
 ptc -L $pt_lib_path $src_path
