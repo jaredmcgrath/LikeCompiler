d60d511be54db9979010854e6243064a2e07a77a
jaredmcgrath <jared.mcgrath@queensu.ca>
2021-04-13 14:41:22 -0400

    Create a branch with instructor solution for all previous phases

--- testSuite/Phase4Tests/5-stringsAssign_positive.s
+++ testSuite/Phase4Tests/5-stringsAssign_positive.s
@@ -7,21 +7,46 @@ ptmain:    pushl   %ebp
         movl    %esp, %ebp    
     movl    $2,u+0
     .data    
-s16:    .asciz  "string"
+s15:    .asciz  "strinval"
+    .text    
+    movl    $4,n
+    pushl    u+0
+    lea    s15,%eax
+    pushl    %eax
+    pushl    $0
+    call    pttrap109
+    addl    $12,%esp
+    incl    n
+    pushl    u+0
+    call    pttrap6
+    addl    $4,%esp
+    .data    
+s58:    .asciz  "string"
     .text    
     pushl    %eax
     pushl    %ebx
     pushl    %ecx
     pushl    %edx
     pushl    u+4
-    lea    s16,%eax
+    lea    s58,%eax
     pushl    %eax
     call    pttrap101
     popl    %edx
     popl    %ecx
     popl    %ebx
     popl    %eax
-    movl    $3,n
+    movl    $8,n
+    pushl    u+0
+    lea    u+4,%eax
+    pushl    %eax
+    pushl    $0
+    call    pttrap109
+    addl    $12,%esp
+    incl    n
+    pushl    u+0
+    call    pttrap6
+    addl    $4,%esp
+    incl    n
     call    pttrap0
     leave    
     ret    
