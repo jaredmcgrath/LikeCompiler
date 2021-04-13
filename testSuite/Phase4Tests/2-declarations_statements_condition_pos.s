541afc3f3d411ca050c2d3c3c79593faf2f1e814
AlexanderIngham <alexingham3@gmail.com>
2021-04-13 14:41:45 -0400

    Added more test cases

---
+++ testSuite/Phase4Tests/2-declarations_statements_condition_pos.s
@@ -0,0 +1,10 @@
+    .data    
+    .comm    u,6
+n:    .long    0
+    .text    
+    .globl    ptmain
+ptmain:    pushl   %ebp
+        movl    %esp, %ebp    
+    movl    $2,u+0
+    leave    
+    ret    
